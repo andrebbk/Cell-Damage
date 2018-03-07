b2cf9bcde62724ae3c46c86069b6822a64ed7ed3
André Silva <andre.f.m.silva@hotmail.com>
2018-03-07 18:31:22 +0000

    Teleportation, explosions and nitro

---
+++ Cell Damage/Assets/Scripts/ChangeCarPlayerPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCarPlayerPosition : MonoBehaviour {
+
+	bool _flag = false;
+
+	void Update()
+	{
+		if(_flag)
+			GetComponent<Rigidbody> ().isKinematic = false;
+	}
+
+	public void ChangePositionFor(Vector3 spawnPos, GameObject spawnRot)
+	{
+		_flag = false;
+		GetComponent<Rigidbody> ().isKinematic = true;
+		StartCoroutine (MudarFlag ());
+		transform.position = spawnPos;
+		transform.rotation = spawnRot.transform.rotation;
+		GetComponent<Rigidbody>().velocity = Vector3.zero;
+		GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+
+	}
+
+
+	IEnumerator MudarFlag()
+	{
+		yield return new WaitForSeconds (1);
+		_flag = true;
+	}
+}
---
+++ Cell Damage/Assets/Scripts/Teletransport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teletransport : MonoBehaviour {
+
+	public GameObject _Target;
+
+	void OnTriggerEnter(Collider col) {
+
+		GameObject Pla = GameObject.FindGameObjectWithTag("carroPlayer");
+
+		if (col.gameObject.name == Pla.name) 
+		{
+			Pla.GetComponent<ChangeCarPlayerPosition>().ChangePositionFor(_Target.transform.position, _Target);
+		}
+
+	}
+
+}
