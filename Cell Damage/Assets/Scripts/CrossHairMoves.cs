f89912dfec19e403297fb1b27e9d207e9825fbbd
André Silva <andre.f.m.silva@hotmail.com>
2018-03-10 22:55:22 +0000

    First version with guns

---
+++ Cell Damage/Assets/Scripts/CrossHairMoves.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHairMoves : MonoBehaviour {
+
+
+	//the object to move
+	public Transform moveThis;
+	//the layers the ray can hit
+	public LayerMask hitLayers;
+
+	void Update()
+	{
+		Vector3 mouse = Input.mousePosition;
+		Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+		RaycastHit hit;
+		if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers))
+		{
+			moveThis.transform.position = hit.point;
+		}
+	}
+}
---
+++ Cell Damage/Assets/Scripts/GunsLooking.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunsLooking : MonoBehaviour {	
+
+	void LateUpdate() {
+		Vector3 fwd = Camera.main.transform.forward;
+		fwd.y = 0;
+
+		if (fwd.sqrMagnitude != 0.0f) {
+			fwd.Normalize();
+			transform.LookAt(transform.position + fwd);
+		}
+	}
+}
